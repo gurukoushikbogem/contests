6e784dc579aa8219038041f34a114d99e246f0b5
Guru Koushik Bogem <gurukoushik08@gmail.com>
2025-12-31 23:03:15 +0530

    codechef

---
+++ c++/codechef/219/Ones_and_Zeroes_II.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define INF 1e15
+#define SORT(v) sort((v).begin(), (v).end())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define mod (long long)(1e9 + 7)
+#define vi vector<long long>
+#define vvi vector<vector<long long>>
+#define vc vector<char>
+#define vpii vector<pair<long long, long long>>
+#define endl "\n"
+#define mpii map<long long, long long>
+#define umpii unordered_map<long long, long long>
+#define mpci map<char, long long>
+using namespace std;
+template <class T>
+void input(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+
+template <class T>
+void print(vector<T> &a)
+{
+    for (auto &x : a)
+        cout << x << " ";
+    cout << endl;
+}
+
+template <class T>
+void print(T x) { cout << x << " "; }
+
+template <class T>
+void println(T x) { cout << x << "\n"; }
+
+
+void solution()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int c = 0;
+    vi v;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+        {
+            c++;
+            v.pb(i);
+        }
+    }
+    int ans = min(c * 2, n);
+    int temp = 0, tt = 0;
+    int c2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '0')
+        {
+            temp--;
+        }
+        else
+        {
+            temp++;
+        }
+        if (temp < 0)
+        {
+            auto it = upper_bound(v.begin(), v.end(), i);
+            if (it != v.end())
+            {
+                tt += (*it - i);
+                s[*it] = '0';
+                v.erase(it);
+                temp = 1;
+            }
+        }
+    }
+    cout << ans << " " << tt << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solution();
+    }
+    return 0;
+}
